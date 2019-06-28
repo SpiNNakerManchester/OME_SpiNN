6144c71b7e8bc088480be00137b5a15e757bbe40
alan-stokes <alan.barry.stokes@gmail.com>
2019-06-28 11:27:46 +0100

    fixed make files so make actually works

--- c_models/drnl_vertex.mk
+++ c_models/drnl_vertex.mk
@@ -4,6 +4,6 @@ SOURCES = drnl_vertex/SpiNNakEar_DRNL.c
 
 CFLAGS += -DSPINNAKER
 
-APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../model_binaries/)/
+APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../model_binaries/)/
 
 include $(SPINN_DIRS)/make/local.mk
--- c_models/ihcan_vertex.mk
+++ c_models/ihcan_vertex.mk
@@ -4,6 +4,6 @@ SOURCES = ihcan_vertex/SpiNNakEar_IHCAN.c
 
 CFLAGS += -DSPINNAKER
 
-APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../../model_binaries/)/
+APP_OUTPUT_DIR := $(abspath $(CURRENT_DIR)../model_binaries/)/
 
 include $(SPINN_DIRS)/make/local.mk
