0855c0902819ce70605553ee41c3e525c7649198
alan-stokes <alan.barry.stokes@gmail.com>
2019-07-24 11:41:19 +0100

    cleaned up an vertex in c code

--- c_models/src/an_node/AN_group_node.h
+++ c_models/src/an_node/AN_group_node.h
@@ -1,26 +1,51 @@
-/*
- * IHC_AN.h
- *
- *  Created on: 1 Nov 2016
- *      Author: rjames
- */
 
 #ifndef AN_group_node_H_
 #define AN_group_node_H_
 
-#define TIMER2_CONF        0x82
-#define TIMER2_LOAD        0
+//! \ brief marker to fill out api for packet sending without payload
+#define PARAM_FILLER 0
 
-#define max(a,b) \
-   ({ __typeof__ (a) _a = (a); \
-       __typeof__ (b) _b = (b); \
-     _a > _b ? _a : _b; })
-
-typedef struct key_mask_table {
+//! \brief table entry
+typedef struct key_mask_table_entry {
     uint32_t key;
     uint32_t mask;
     uint32_t offset;
 } key_mask_table_entry;
+
+//! \brief params from parameter region
+typedef struct params_struct {
+    int n_children;
+    int has_key;
+    int key;
+    int is_final_row;
+    int n_atoms;
+} params_struct;
+
+//! \brief key map struct
+typedef struct key_map_struct {
+    key_mask_table_entry *entries;
+} key_map_struct;
+
+//! \brief priorities
+typedef enum priorities {
+    MC = -1, TIMER = 0, USER = 1, SDP_PRIORITY = 1,
+    DMA_TRANSFER_DONE_PRIORITY = 0
+} priorities;
+
+//! \brief provenance data
+typedef enum provenance_items {
+    N_SPIKES_TRANSMITTED
+} provenance_items;
+
+//! \brief data spec regions
+typedef enum regions {
+    SYSTEM,
+    PARAMS,
+    KEY_MAP,
+    PROVENANCE
+} regions;
+
+
 
 
 #endif /*AN_group_node_H_ */
--- c_models/src/an_node/SpiNNakEar_AN_group_node.c
+++ c_models/src/an_node/SpiNNakEar_AN_group_node.c
@@ -3,7 +3,8 @@
  Name        : SpiNNakEar_IHCAN.c
  Author      : Robert James
  Version     : 1.0
- Description : Inner Hair Cell + Auditory Nerve model for use in SpiNNakEar system
+ Description : Inner Hair Cell + Auditory Nerve model for use in SpiNNakEar
+               system
  ============================================================================
  */
 
@@ -20,106 +21,50 @@
 #include <simulation.h>
 #include <debug.h>
 
-//data spec regions
-typedef enum regions {
-    SYSTEM,
-    PARAMS,
-    RECORDING,
-    PROFILER}regions;
-
-// The parameters to be read from memory
-enum params {
-    N_IHCS,
-    AN_KEY,
-    IS_KEY,
-    IS_FINAL,
-    N_ATOMS,
-    IHC_KEYS
-};
-
-uint32_t an_key;
-uint32_t is_key;
-uint32_t n_ihcs;
-uint32_t is_final;
+//! \brief the number of times this core has fired
 uint32_t spike_count = 0;
-uint32_t n_atoms;
-bool final_r2s;
-static key_mask_table_entry *key_mask_table;
-uint32_t coreID,chipID;
 
+//! \brief the key map
+static key_map_struct *key_mask_table;
+
+//! \brief params
+static params_struct parameters;
+
+//************** simulation interface demands *******//
+//! \brief how many ticks done
 static uint32_t simulation_ticks = 0;
+
+//! \brief time to run to
 uint32_t time;
 
-bool app_complete = false;
+//! \brief infinite run pointer
+static uint32_t infinite_run;
 
-//application initialisation
-bool app_init(uint32_t *timer_period)
-{
-	io_printf (IO_BUF, "[core %d] -----------------------\n", coreID);
-	io_printf (IO_BUF, "[core %d] an group starting simulation\n", coreID);
-	//obtain data spec
-	address_t data_address = data_specification_get_data_address();
-    io_printf(IO_BUF,"data_address=%d\n",data_address);
+//! \brief locate new key and transmit
+//! \param[in] spike: the key received
+//! \param[in] null_a: forced by api.
+void key_search_and_send(uint spike, uint null_a) {
+    use(null_a);
 
-        // Get the timing details and set up the simulation interface
-    if (!simulation_initialise(
-            data_specification_get_region(SYSTEM, data_address),
-            APPLICATION_NAME_HASH, timer_period, &simulation_ticks,
-            NULL, 1, 0)) {
-        return false;
+    if (parameters.is_final_row){
+        spike_count ++;
     }
-
-    //get parameters
-    address_t params = data_specification_get_region(
-                                        PARAMS,data_address);
-
-    n_ihcs = params[N_IHCS];
-    io_printf(IO_BUF,"n_ihcs=%d\n",n_ihcs);
-
-    uint32_t n_ihc_key_bytes =  n_ihcs * sizeof(key_mask_table_entry);
-    io_printf(IO_BUF,"n_ihc_key_bytes=%d\n",n_ihc_key_bytes);
-    key_mask_table = (key_mask_table_entry *)spin1_malloc(n_ihc_key_bytes);
-    if (key_mask_table == NULL){
-        io_printf (IO_BUF, "failed to allocate memory\n");
-        app_end();
-    }
-
-    spin1_memcpy(key_mask_table, &params[IHC_KEYS],n_ihc_key_bytes);
-
-    for (uint i=0;i<n_ihcs;i++){
-        io_printf(IO_BUF,"ihc key:0x%x mask:0x%x offset:%d\n",key_mask_table[i].key,key_mask_table[i].mask,key_mask_table[i].offset);
-    }
-    an_key = params[AN_KEY];
-    is_key = params[IS_KEY];
-    is_final = params[IS_FINAL];
-    n_atoms = params[N_ATOMS];
-    io_printf(IO_BUF,"an_key=%d\n",an_key);
-    io_printf(IO_BUF,"is_key=%d\n",is_key);
-    io_printf(IO_BUF,"is_final=%d\n",is_final);
-    io_printf(IO_BUF,"n_atoms=%d\n",n_atoms);
-    final_r2s = 0;
-}
-
-void key_search_and_send(uint32_t spike,uint null){
-//    io_printf(IO_BUF,"r\n");
-    if (is_final) spike_count++;
     //search through ihc_keys for the rx key
     uint32_t imin = 0;
-    uint32_t imax = n_ihcs;
+    uint32_t imax = parameters.n_children;
     uint32_t imid;
     key_mask_table_entry entry;
     while (imin < imax) {
-//    for (uint imid=0;imid<imax;imid++){
         imid = (imax + imin) >> 1;
-        entry = key_mask_table[imid];
+        entry = key_mask_table->entries[imid];
         if ((spike & entry.mask) == entry.key){
-            uint32_t neuron_id = entry.offset+ (spike & ~entry.mask);
-            if(neuron_id>=n_atoms){
-                log_info("incorrect neuron ID generated %d",neuron_id);
+            int neuron_id = entry.offset + (spike & ~entry.mask);
+            if(neuron_id >= parameters.n_atoms){
+                log_error("incorrect neuron ID generated %d", neuron_id);
                 rt_error(RTE_SWERR);
             }
-//            io_printf(IO_BUF,"id: %d txk: 0x%x\n",neuron_id,an_key|neuron_id);
-            while(!spin1_send_mc_packet(an_key|neuron_id,0,NO_PAYLOAD)){
+            while(!spin1_send_mc_packet(
+                    parameters.key | neuron_id, PARAM_FILLER, NO_PAYLOAD)){
                 spin1_delay_us(1);
             }
             return;
@@ -132,80 +77,141 @@ void key_search_and_send(uint32_t spike,uint null){
             imax = imid;
         }
     }
-    io_printf(IO_BUF,"key %d not found!\n",(uint32_t)(spike & entry.mask));
+    log_error("key %d not found!\n", spike);
 }
 
-//void user_event_callback(uint null,uint null){
-//
-//
-//}
+//! \brief mc packet reception
+//! \param[in] mc_key: the key of the mc packet
+//! \param[in] null: the forced api param.
+void spike_rx(uint mc_key, uint null) {
+    use(null);
 
-void spike_rx(uint32_t mc_key, uint null){
-//    io_printf(IO_BUF,"k:%d\n",mc_key);
-    if (is_key)spin1_schedule_callback(key_search_and_send,mc_key,NULL,1);
-//    if (is_key){
-//        key_search_and_send(mc_key,NULL);
-//        }
+    // only going to process and retransmit if i have a key
+    if (parameters.has_key) {
+        spin1_schedule_callback(
+            key_search_and_send, mc_key, PARAM_FILLER, USER);
+    }
 }
 
-void app_end()
-{
-//    if (!is_final){
-//        while (!spin1_send_mc_packet(an_key, 0, WITH_PAYLOAD)) {
-//            spin1_delay_us(1);
-//        }
-//    }
-//    spin1_exit(0);
-    log_info("total simulation ticks = %d",
-          simulation_ticks);
-    if(is_final)log_info("total spike count = %d",spike_count);
-    app_complete = true;
-    simulation_ready_to_read();
-    io_printf (IO_BUF, "spinn_exit\n");
+//! \brief stores provenance data
+//! \param[in] provenance_region: the sdram location for the prov data.
+void _store_provenance_data(address_t provenance_region) {
+    log_debug("writing other provenance data");
+
+    // store the data into the provenance data region
+    if (parameters.is_final_row) {
+        provenance_region[N_SPIKES_TRANSMITTED] = spike_count;
+    }
+
+    log_debug("finished other provenance data");
 }
 
-void mc_payload_rx_callback(uint null_a, uint null_b){
-//    app_end();
-    log_info("command from OME received %d",final_r2s);
-    spin1_schedule_callback(app_end,NULL,NULL,2);
-//    if(final_r2s)spin1_schedule_callback(app_end,NULL,NULL,2);
-//    else final_r2s = 1;
-}
-
+//! \brief timer for mapping to auto pause and resume
+//! \param[in] null_a: forced by api
+//! \param[in] null_b: forced by api
 void count_ticks(uint null_a, uint null_b){
+    use(null_a);
+    use(null_b);
 
     time++;
-    if (time>simulation_ticks && !app_complete)spin1_schedule_callback(app_end,NULL,NULL,2);
+    // If a fixed number of simulation ticks are specified and these have passed
+    if (infinite_run != TRUE && time >= simulation_ticks) {
+
+        // handle the pause and resume functionality
+        simulation_handle_pause_resume(NULL);
+
+         // Subtract 1 from the time so this tick gets done again on the next
+        // run
+        time -= 1;
+        simulation_ready_to_read();
+        return;
+    }
 
 }
 
-void c_main()
-{
-    // Get core and chip IDs
-    coreID = spin1_get_core_id ();
-    chipID = spin1_get_chip_id ();
+
+//! \brief application initialisation
+//! \param[in] timer_period: pointer to the timer period
+//! \return: bool saying if init was successful or not
+bool app_init(uint32_t *timer_period) {
+	//obtain data spec
+	data_specification_metadata_t *data_address =
+	    data_specification_get_data_address();
+    log_info("data_address = %d\n", data_address);
+
+        // Get the timing details and set up the simulation interface
+    if (!simulation_initialise(
+            data_specification_get_region(SYSTEM, data_address),
+            APPLICATION_NAME_HASH, timer_period, &simulation_ticks,
+            &infinite_run, &time, SDP_PRIORITY, DMA_TRANSFER_DONE_PRIORITY)) {
+        return false;
+    }
+
+    //get parameters
+    spin1_memcpy(
+        &parameters, data_specification_get_region(PARAMS, data_address),
+        sizeof(params_struct));
+
+    //! print out params data
+    log_info("n_ihcs = %d", parameters.n_children);
+    log_info("an_key = %d", parameters.key);
+    log_info("is_key = %d", parameters.has_key);
+    log_info("is_final = %d", parameters.is_final_row);
+    log_info("n_atoms = %d", parameters.n_atoms);
+
+    // how many bytes represents the array of keys
+    int n_ihc_key_bytes =
+        parameters.n_children * sizeof(key_mask_table_entry);
+
+    // read in array of keys
+    log_info("n_ihc_key_bytes = %d", n_ihc_key_bytes);
+    key_mask_table = (key_map_struct*) spin1_malloc(n_ihc_key_bytes);
+    if (key_mask_table == NULL) {
+        log_error("failed to allocate memory");
+        return false;
+    }
+    spin1_memcpy(
+        key_mask_table,  data_specification_get_region(KEY_MAP, data_address),
+        n_ihc_key_bytes);
+
+    // print key array
+    for (int i = 0; i < parameters.n_children; i++) {
+        log_info(
+            "ihc key:0x%x mask:0x%x offset:%d",
+            key_mask_table->entries[i].key, key_mask_table->entries[i].mask,
+            key_mask_table->entries[i].offset);
+    }
+
+    // sort out provenance data
+    simulation_set_provenance_function(
+        _store_provenance_data,
+        data_specification_get_region(PROVENANCE, data_address));
+
+    return true;
+}
+
+//! \brief entrance method
+void c_main() {
     // Load DTCM data
     uint32_t timer_period;
 
     // initialise the model
     if (!app_init(&timer_period)){
-    rt_error(RTE_API);
+        log_error("failed to init the application. shutting down");
+        rt_error(RTE_API);
     }
 
     // Start the time at "-1" so that the first tick will be 0
     time = UINT32_MAX;
 
     // Set timer tick (in microseconds)
-    log_info("setting timer tick callback for %d microseconds",
-          timer_period);
+    log_info("setting timer tick callback for %d microseconds", timer_period);
     spin1_set_timer_tick(timer_period);
 
     //setup callbacks
-    spin1_callback_on (MC_PACKET_RECEIVED,spike_rx,-1);
-//    spin1_callback_on (MCPL_PACKET_RECEIVED,mc_payload_rx_callback,-1);
-    spin1_callback_on (TIMER_TICK,count_ticks,0);
+    spin1_callback_on(MC_PACKET_RECEIVED, spike_rx, MC);
+    spin1_callback_on(TIMER_TICK, count_ticks, TIMER);
 
-//    spin1_start (SYNC_WAIT);
+
     simulation_run();
-
 }
