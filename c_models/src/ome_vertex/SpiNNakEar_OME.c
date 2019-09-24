a2940a0151bece4fcff04d88fa2cff3d6226c17a
alan-stokes <alan.barry.stokes@gmail.com>
2019-09-24 09:16:38 +0100

    rats

--- c_models/src/ome_vertex/SpiNNakEar_OME.c
+++ c_models/src/ome_vertex/SpiNNakEar_OME.c
@@ -1,10 +1,22 @@
 /*
- ============================================================================
- Name        : SpiNNakEar_OME.c
- Author      : Robert James
- Version     : 1.0
+ * Copyright (c) 2019-2020 The University of Manchester
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
  Description : Outer and middle ear model for use in SpiNNakEar system
- ============================================================================
  */
 
 #include <stdio.h>
